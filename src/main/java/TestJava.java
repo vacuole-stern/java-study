ea9e93c0d47b7ff196f8fe40e8b3cb43a35965df
vacuole <vacuole@yeah.net>
2022-09-26 23:05:19 +0800

    新增测试文件

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class TestJava {
+
+    @Test
+    public void main(){
+        Method();
+    }
+
+    public void Method(){
+        int a = 1;
+        int b = 1;
+        int OutResult = a + b;
+        System.out.println(OutResult);
+
+    }
+
+}
